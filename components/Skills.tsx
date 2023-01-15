dbfb0575461b61c43d1347eb971f2340d8d6655c
Antrix Saini <sainiantrix@gmail.com>
2023-01-15 15:40:48 +0530

    Project Section added

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function Skills({}: Props) {
   return (
-    <div className="relative h-screen flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <div className="relative h-screen flex flex-col overflow-hidden text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
       <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
